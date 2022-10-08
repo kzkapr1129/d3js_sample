82e4d243cd71777d7dfcde62d55840a0cd40dded
中山一輝 <nakayama@mackbook-air.local>
2022-10-08 10:30:35 +0900

    目盛り線の描画

--- src/components/Sample8.jsx
+++ src/components/Sample8.jsx
@@ -1,91 +1,37 @@
 import * as d3 from "d3";
 import { useEffect, useRef } from "react";
+import { makeScaleData, makeScaleMaterial } from "./scaleMaker";
 
 const redraw = (d3Container) => {
-  //const svg = d3.select(d3Container.current);
-  //let node = svg.node();
-  //let parentNode = node.parentNode;
+  const svg = d3.select(d3Container.current);
+  let node = svg.node();
+  let parentNode = node.parentNode;
 
-  //let padding = 5;
-  //let contentRect = {x1: padding, y1: padding, x2: parentNode.clientWidth - padding, y2: parentNode.clientHeight - padding};
+  let padding = 5;
+  let contentRect = {x1: padding, y1: padding, x2: parentNode.clientWidth - padding, y2: parentNode.clientHeight - padding};
+  let xScale = d3.scaleLinear().domain([-1, 1]).range([contentRect.x1, contentRect.x2]);
+  let yScale = d3.scaleLinear().domain([-1, 1]).range([contentRect.y2, contentRect.y1]);
 
-  //let xrange = [0.3234, 0.1234];
+  let scaleMaterial = makeScaleMaterial([-1, 1]);
+  let majorScaleData = makeScaleData(scaleMaterial.min, scaleMaterial.max, scaleMaterial.maxDigitMask);
+  let mainorScaleData = makeScaleData(scaleMaterial.min, scaleMaterial.max, scaleMaterial.maxDigitMask/10);
+  mainorScaleData = mainorScaleData.filter(v => !majorScaleData.includes(v));
 
-  const makeScaleMaterial = (array) => {
-    const min = array.reduce((a, b) => Math.min(a, b));
-    const max = array.reduce((a, b) => Math.max(a, b));
+  let mainorScales = svg.select("#scale-bar").select("#scale-minor").selectAll("line").data(mainorScaleData);
+  mainorScales.enter().append("line").merge(mainorScales)
+    .attr("x1", d => xScale(d))
+    .attr("y1", yScale(-1))
+    .attr("x2", d => xScale(d))
+    .attr("y2", yScale(-0.9))
+    .attr("stroke", "black");
 
-    const calcMaxIntDigitMask = (v) => {
-      if (!v || v === "0") return 0;
-      let reg = v.match(/^0+/);
-      let correction = reg ? v.length - reg[0].length - 1: v.length - 1;
-      return Math.pow(10, correction);
-    }
-
-    const calcMaxDecimalDigitMask = (v) => {
-      if (!v || v === "0") return 0;
-      // 小数点の最大桁数をマスクする値を返却する
-      let reg = v.match(/^0+/);
-      let correction = reg ? reg[0].length + 1 : 1;
-      return 1.0 / Math.pow(10, correction);
-    }
-
-    const makeScaleMaterialImpl = (v) => {
-      const absValue = Math.abs(v);
-      // 目盛りを作成するための材料となる値を計算する
-      const valueComponents = v.toString().split(".");
-      switch (valueComponents.length) {
-        // 空文字の場合
-        case 0: return {maxDigitMask: 0};
-        // 整数値、または整数値を省略した小数 例) .123
-        case 1: {
-          if (absValue < 1) {
-            // 指定数値が0~1の場合
-            let maxDigitMask = calcMaxDecimalDigitMask(valueComponents[0]);
-            return {maxDigitMask};
-          }
-          let maxDigitMask = calcMaxIntDigitMask(valueComponents[0]);
-          return {maxDigitMask};
-        }
-        // 整数値+少数の場合
-        case 2: {
-          if (v < 1) {
-            // 指定数値が0~1の場合
-            let maxDigitMask = calcMaxDecimalDigitMask(valueComponents[1]);
-            return {maxDigitMask};
-          }
-          // 1以上の場合は整数のみを対象とする
-          let maxDigitMask = calcMaxIntDigitMask(valueComponents[0]);
-          return {maxDigitMask};
-        }
-        default:
-          return {maxDigitMask: 0};
-      }
-    };
-    return {...{min, max}, ...makeScaleMaterialImpl(max-min)};
-  };
-
-  const countDecimalDigit = (v) => {
-    let valueComponents = v.toString().split(".");
-    switch (valueComponents.length) {
-      case 0: return 0;
-      case 1: return (v < 0) ? valueComponents[0].length : 0;
-      case 2: return valueComponents[1].length;
-      default: return 0;
-    }
-  }
-
-  try {
-    let scaleMaterial = makeScaleMaterial([13.5, 13.71]);
-    let scaleStart = parseInt(scaleMaterial.min / scaleMaterial.maxDigitMask, 10) * scaleMaterial.maxDigitMask;
-    let scaleEnd   = parseInt(scaleMaterial.max / scaleMaterial.maxDigitMask, 10) * scaleMaterial.maxDigitMask;
-    let numScale = Math.ceil((scaleEnd - scaleStart) / scaleMaterial.maxDigitMask) + 1;
-    let fixedParam = countDecimalDigit(scaleMaterial.maxDigitMask);
-    let scale = [...Array(numScale).keys()].map((v) => (scaleStart + v * scaleMaterial.maxDigitMask).toFixed(fixedParam));
-    console.log(scaleMaterial.maxDigitMask, fixedParam, scale);
-  } catch {
-
-  }
+  let majorScales = svg.select("#scale-bar").select("#scale-major").selectAll("line").data(majorScaleData);
+  majorScales.enter().append("line").merge(majorScales)
+    .attr("x1", d => xScale(d))
+    .attr("y1", yScale(-1))
+    .attr("x2", d => xScale(d))
+    .attr("y2", yScale(-0.8))
+    .attr("stroke", "red");
 }
 
 export const Sample8 = () => {
@@ -103,7 +49,12 @@ export const Sample8 = () => {
     <div>
       <h3>this is Sample8</h3>
       <div style={{ width: "80%", height: "300px", background: "rgb(200, 200, 200)"}}>
-        <svg ref={d3Container} height="100%" width="100%"/>
+        <svg ref={d3Container} height="100%" width="100%">
+          <g id="scale-bar">
+            <g id="scale-minor"/>
+            <g id="scale-major"/>
+          </g>
+        </svg>
       </div>
     </div>
 
